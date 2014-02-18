0056cec369ebd48a1101a756c8221be319aa0f5f
Mauricio Leal <maltron@gmail.com>
2014-02-18 00:42:32 -0300

    Updating confidential information

--- glassfish-create-resources.sh
+++ glassfish-create-resources.sh
@@ -1,9 +1,11 @@
+# Run the first script, so the variables get loaded into memory
+source glassfish.sh
 # Creating Database and inserting some data in it
-mysql -u mauricio -p < create-database.sql
+mysql --user=${MYSQL_USERNAME} --password=${MYSQL_PASSWORD} < create-database.sql
 # Symbolic link for MySQL Driver into GlassFish
-ln -sv /home/mauricio/java/api/jdbc/mysql/mysql.jar /home/mauricio/java/server/AS/glassfish/open/current/glassfish/domains/domain1/lib/
+ln -sv ${MYSQL_JDBC_FILE} ${GLASSFISH_LOCATION}/glassfish/domains/domain1/lib/
 # Creating JDBC Connection Pool
-asadmin create-jdbc-connection-pool --restype javax.sql.DataSource --datasourceclassname com.mysql.jdbc.jdbc2.optional.MysqlDataSource --property user=mauricio:password=maltron:DatabaseName=PERSON_DATA:ServerName=localhost:port=3306 PersonPool
+asadmin create-jdbc-connection-pool --restype javax.sql.DataSource --datasourceclassname com.mysql.jdbc.jdbc2.optional.MysqlDataSource --property user=${MYSQL_USERNAME}:password=${MYSQL_PASSWORD}:DatabaseName=PERSON_DATA:ServerName=localhost:port=3306 PersonPool
 # Creating JNDI regarding the Connection Pool
 asadmin create-jdbc-resource --connectionpoolid PersonPool "jdbc/Person"
 # Creating JMS for Notification
